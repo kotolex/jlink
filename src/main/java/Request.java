b950e677911b6acd755832884963aaa20c72e9dc
LexDraven <Lexman2@ya.ru>
2017-11-05 14:30:56 +0500

    enable java 8 support, add method for getting page source, add method for printing exceptions

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Request {
     private final String link;
@@ -10,18 +15,53 @@ public class Request {
     }
 
     public boolean isSuccess() {
+        Optional<HttpURLConnection> optional = connection();
+        if (!optional.isPresent()) {
+            return false;
+        }
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(link).openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
+            HttpURLConnection connection = optional.get();
             boolean result = connection.getResponseCode() == success;
             connection.disconnect();
             return result;
-        } catch (Exception e) {
-            System.err.println(link + " raise exception " + e.getMessage());
+        } catch (IOException e) {
+            printException(e);
             return false;
         }
     }
 
+    public List<String> pageSource() {
+        Optional<HttpURLConnection> optional = connection();
+        LinkedList<String> linkedList = new LinkedList<>();
+        if (optional.isPresent()) {
+            try {
+                HttpURLConnection connection = optional.get();
+                Scanner scanner = new Scanner(connection.getInputStream());
+                while (scanner.hasNext()) {
+                    linkedList.add(scanner.next());
+                }
+                connection.disconnect();
+            } catch (Exception e) {
+                printException(e);
+            }
+        }
+        return linkedList;
+    }
+
+    private Optional<HttpURLConnection> connection() {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(link).openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            return Optional.of(connection);
+        } catch (Exception e) {
+            printException(e);
+            return Optional.empty();
+        }
+    }
+
+    private void printException(Exception ex) {
+        System.err.println(link + " raise exception " + ex.getMessage());
+    }
 
 }
