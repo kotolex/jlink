1d14ba4998f12a972231c464fe45c7cdcbae914a
LexDraven <Lexman2@ya.ru>
2017-11-06 18:49:14 +0500

    add xml as a source? refactoring

--- src/main/java/LinkVisitor.java
+++ src/main/java/LinkVisitor.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.stream.Collectors;
 
@@ -8,6 +9,7 @@ public class LinkVisitor implements ThreadWaiting {
     private ConcurrentHashMap<String, String> broken;
     private ConcurrentSkipListSet<String> visited;
     private ConcurrentSkipListSet<String> checked;
+    private ConcurrentLinkedQueue<Thread> threads;
     private final String mainDomain;
 
     public LinkVisitor(String mainDomain) {
@@ -15,6 +17,7 @@ public class LinkVisitor implements ThreadWaiting {
         visited = new ConcurrentSkipListSet<>();
         checked = new ConcurrentSkipListSet<>();
         broken = new ConcurrentHashMap<>();
+        threads = new ConcurrentLinkedQueue<>();
     }
 
     public void start() {
@@ -24,6 +27,7 @@ public class LinkVisitor implements ThreadWaiting {
         while (!isAllThreadsStopped()) ;
         System.out.println("Checked links: " + checked.size());
         System.out.println("Broken links: " + broken.size());
+        System.out.println("Visited links: " + visited.size());
         System.out.println("Total time: " + (System.currentTimeMillis() - startTime) / 1000);
         if (broken.size() > 0) {
             for (Map.Entry pair : broken.entrySet()) {
@@ -35,26 +39,36 @@ public class LinkVisitor implements ThreadWaiting {
     private void startThread(Runnable runnable) {
         Thread thread = new Thread(runnable);
         thread.setDaemon(true);
+        threads.add(thread);
         thread.start();
     }
 
     @Override
     public boolean isAllThreadsStopped() {
-        return Thread.activeCount() < 3;
+        if (Thread.activeCount() < 3) {
+            for (Thread thread : threads) {
+                if (thread.isAlive()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
 
     class Inspector implements Runnable {
         private List<String> links;
-        private final String mainURl;
+        private final String mainUrl;
 
-        public Inspector(List<String> links, String mainURl) {
+        public Inspector(List<String> links, String mainUrl) {
             this.links = links;
-            this.mainURl = mainURl;
+            this.mainUrl = mainUrl;
         }
 
         private boolean isSource(String link) {
-            if (link.endsWith(".js") || link.endsWith(".rss") || link.endsWith(".jpg") || link.endsWith(".png") || link.endsWith(".css")) {
+            if (link.endsWith(".js") || link.endsWith(".rss") || link.endsWith(".jpg")
+                    || link.endsWith(".png") || link.endsWith(".css") || link.endsWith(".xml")) {
                 return true;
             }
             return false;
@@ -66,14 +80,18 @@ public class LinkVisitor implements ThreadWaiting {
                 if (!checked.contains(link)) {
                     checked.add(link);
                     if (!new Request(link).isSuccess()) {
-                        broken.putIfAbsent(link, mainURl);
+                        broken.putIfAbsent(link, mainUrl);
                     } else {
-                        if (link.startsWith(mainURl) && !visited.contains(link) && !isSource(link)) {
+                        if (needToVisit(link)) {
                             startThread(new Visitor(link));
                         }
                     }
                 }
             }
+        }
+
+        private boolean needToVisit(String link) {
+            return link.startsWith(mainUrl) && !visited.contains(link) && !isSource(link);
         }
     }
 
